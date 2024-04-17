53bc3ea8b90a9dadcc1eaac0fde6ef5a0357ebaa
Theodor Oprea <theodoroprea@Theodors-MacBook-Pro.local>
2024-04-16 18:28:58 -0700

    try again

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 import styles from "../styles/Images.module.scss";
 import { useEffect, useState } from "react";
-import Navbar from "@/components/navbar";
 import About from "@/components/about";
 import Parallax from "@/components/parallax";
 import ContactUs from "@/components/contactUs";
