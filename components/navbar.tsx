30e47d76698f26e004f71100b614dca5a5110c1c
Theodor Oprea <theodoroprea@Theodors-MacBook-Pro.local>
2024-07-12 16:51:31 -0700

    Added products page as well as checkout + functionality

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,38 @@
 import styles from "../styles/Navbar.module.scss";
 
-const Navbar = () => {
+interface NavbarProps {
+  page: string;
+}
+
+const Navbar = (props: NavbarProps) => {
+  const ConditionalNavbarComponent = () => {
+    if (props.page === "home") {
+      return (
+        <nav className={styles.nav}>
+          <a href="#about">About</a>
+          <a href="/products">Products</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      );
+    } else if (props.page === "products") {
+      return (
+        <nav className={styles.nav}>
+          <a href="/">Home</a>
+        </nav>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Page Not Found</h1>
+          <p>We couldn't find the page you're looking for.</p>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className={styles.navbar_container}>
-      <nav className={styles.nav}>
-        <a href="#about">About</a>
-        <a href="#">Products</a>
-        <a href="#contact">Contact</a>
-      </nav>
+      <ConditionalNavbarComponent />
     </div>
   );
 };
---
+++ pages/products.tsx
@@ -0,0 +1,10 @@
+import Parallax from "@/components/parallax";
+import Checkout from "./checkout";
+
+export default function ProductsPage() {
+  return (
+    <div>
+      <Parallax page={"products"} />
+    </div>
+  );
+}
