49cf8942efd9419ff34fbf3db291f896def94461
Theodor Oprea <theodoroprea@Theodors-MacBook-Pro.local>
2024-08-16 17:52:54 -0700

    Changed background

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,17 +9,21 @@ const Navbar = (props: NavbarProps) => {
   const ConditionalNavbarComponent = () => {
     if (props.page === "home") {
       return (
-        <nav className={styles.nav}>
-          <a href="#about">About</a>
-          <Link href="/products">Products</Link>
-          <a href="#contact">Contact</a>
-        </nav>
+        <div className={styles.navbar_container_home}>
+          <nav className={styles.nav}>
+            <a href="#about">About</a>
+            <Link href="/products">Products</Link>
+            <a href="#contact">Contact</a>
+          </nav>
+        </div>
       );
     } else if (props.page === "products") {
       return (
-        <nav className={styles.nav}>
-          <Link href="/">Home</Link>
-        </nav>
+        <div className={styles.navbar_container_products}>
+          <nav className={styles.nav}>
+            <Link href="/">Home</Link>
+          </nav>
+        </div>
       );
     } else {
       return (
@@ -31,11 +35,7 @@ const Navbar = (props: NavbarProps) => {
     }
   };
 
-  return (
-    <div className={styles.navbar_container}>
-      <ConditionalNavbarComponent />
-    </div>
-  );
+  return <ConditionalNavbarComponent />;
 };
 
 export default Navbar;
