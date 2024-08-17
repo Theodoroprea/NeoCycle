ba858accb1ebbc60ebb51322b71eef64c11c55eb
Theodor Oprea <theodoroprea@Theodors-MacBook-Pro.local>
2024-08-16 19:47:34 -0700

    Added tab logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,30 @@ export default function Document() {
         <link
           href="https://fonts.cdnfonts.com/css/baron-neue"
           rel="stylesheet"
-        ></link>
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/fav/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/fav/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/fav/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/fav/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/fav/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
       </Head>
       <body>
         <Main />
