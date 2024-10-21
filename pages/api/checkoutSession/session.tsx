2cda4e32c6e7ee649e7ed76b1ae531687f41f032
Theodor Oprea <theodoroprea@Theodors-MacBook-Pro.local>
2024-10-20 21:23:19 -0700

    Success page created

--- pages/api/checkoutSession/session.tsx
+++ pages/api/checkoutSession/session.tsx
@@ -18,7 +18,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     payment_method_types: ["card"],
     line_items: lineItems,
     mode: "payment",
-    success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+    success_url: `${req.headers.origin}/success`,
     cancel_url: `${req.headers.origin}/products`,
   });
   res.status(200).json({ sessionId: session.id });
