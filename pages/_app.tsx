40f76d172eea0b14ced847fbb40ae9c5b70a9467
Theodor Oprea <theodoroprea@Theodors-MacBook-Pro.local>
2024-12-10 22:01:39 -0800

    Added speed insights :

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import "@/styles/globals.scss";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import CartProvider from "@/context/cartContext";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <CartProvider>
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </CartProvider>
     </>
   );
